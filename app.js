469a86529024fdb1845db88cce5d38488d5ed246
Erin Hinson <erinh@xceleration.com>
2017-07-22 15:20:08 -0400

    style registration form and add registration route

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const multer = require('multer');
+
+// Handle File uploads
+var upload = multer({ dest: 'uploads/' });
+
 const flash = require('connect-flash');
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
+const expressValidator = require('express-validator');
 
 const db = mongoose.connection;
 
@@ -24,11 +29,53 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Handle Sessions
+app.use(
+  session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+  })
+);
+
+// Passport Middleware
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Flash Messages
+
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//Validator
+
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
