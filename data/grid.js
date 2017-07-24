904b8e2a3f452349d8e288196b97462036744495
Erin Hinson <erinh@xceleration.com>
2017-07-24 11:42:10 -0400

    Random words and images fill grid

--- data/grid.js
+++ data/grid.js
@@ -1,9 +1,16 @@
 const axios = require('axios');
 
+var data = [
+  { id: 1, title: '', definition: '', image: 'https://unsplash.it/450/300/?random' },
+  { id: 2, title: '', definition: '', image: 'https://unsplash.it/600/400/?random' },
+  { id: 3, title: '', definition: '', image: 'https://unsplash.it/300/200/?random' },
+  { id: 4, title: '', definition: '', image: 'https://unsplash.it/900/600/?random' }
+];
+
 function getWords() {
   return axios
     .get(
-      'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+      `http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=${data.length}&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`
     )
     .then(function(response) {
       return response.data;
@@ -29,12 +36,12 @@ function getDefinitions(word) {
 }
 
 function matchDefinitions() {
-  return getWords()
+  getWords()
     .then(resp =>
       resp.map((word, index) => {
-        return getDefinitions(word.word).then(resp => {
+        getDefinitions(word.word).then(resp => {
           let defs = resp;
-          return defs.map(def => {
+          defs.map(def => {
             words['definition'] = def.text;
             // foo.push(word.word);
             return words;
@@ -45,11 +52,7 @@ function matchDefinitions() {
     .catch(err => console.error(err));
 }
 
-module.exports.data = [
-  { id: 1, title: '', definition: '', image: 'https://unsplash.it/450/300/?random' },
-  { id: 2, title: '', definition: '', image: 'https://unsplash.it/600/400/?random' },
-  { id: 3, title: '', definition: '', image: 'https://unsplash.it/300/200/?random' },
-  { id: 4, title: '', definition: '', image: 'https://unsplash.it/900/600/?random' }
-];
-
-module.exports.getWords = getWords;
+module.exports = {
+  data: data,
+  getWords: getWords
+};
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const data = require('../data/grid');
-var arrayarray = [];
-var foo = data.getWords().then(res => {
-  res.map(item => {
-    arrayarray.push(item.word);
-  });
-  return arrayarray;
-});
 
-data['words'] = foo;
-console.log(data);
 /* GET home page. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
-  res.render('index', { title: 'Home', data: data.data, words: data.words });
+  var wordarray = [];
+  var foo = data.getWords().then(resp => {
+    resp.map(item => {
+      wordarray.push(item.word);
+    });
+    data['words'] = wordarray;
+    console.log(data);
+    res.render('index', { title: 'Home', data: data.data, words: data.words });
+  });
 });
 
 function ensureAuthenticated(req, res, next) {
